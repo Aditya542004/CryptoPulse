6152587ad4df405c202af0186881fa6a60ad516e
Abhishek Goswami <113699791+Abhishek9793web@users.noreply.github.com>
2025-01-15 21:59:47 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+var request=require('request')
+
+var multer = require('multer')
+var upload = multer()
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.set('view engine', 'ejs')
+app.use(upload.array());
+
+const request = require('request');
+request('https://api.coingecko.com/api/v3/coins/bitcoin', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+      
