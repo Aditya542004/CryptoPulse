8d3e6f2e8b98c83cac9fad19c9df83425d46386f
Abhishek Goswami <113699791+Abhishek9793web@users.noreply.github.com>
2025-01-19 20:06:33 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-var request=require('request')
+var request = require('request')
 
 var multer = require('multer')
 var upload = multer()
@@ -10,24 +10,30 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.set('view engine', 'ejs')
-
+app.use(express.static('public'));
 app.use(upload.array());
 
-let data=" "
+let data = " "
 request('https://api.coingecko.com/api/v3/coins/bitcoin', function (error, response, body) {
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  data=JSON.parse(body)
- 
-  
+  console.log('body:', typeof body); // Print the HTML for the Google homepage.
+  data = JSON.parse(body)
 });
 
+// Use app.get() for routing
 app.get('/', (req, res) => {
-  res.render('index.ejs', {data})
-})
+  res.render('deshboard.ejs'); 
+});
+
+app.get('/index', (req, res) => {
+  res.render('index.ejs',{data}); 
+});
+app.get('/about', (req, res) => {
+  res.render('about.ejs'); 
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
